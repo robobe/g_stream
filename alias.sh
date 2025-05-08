34be8822db5886f6ee51e38867af8b60db0eaa42
robo2020 <robo2020@gmail.com>
2025-05-08 14:19:12 +0000

    demo run

--- alias.sh
+++ alias.sh
@@ -1,3 +1,4 @@
+alias g_stream_demo="tmuxp load /workspace/tmux_scripts/dev.yaml"
 alias dev="tmuxp load src/g_stream/launch/dev.yaml"
 alias dev_pc="tmuxp load src/g_stream/launch/dev_pc_264.yaml"
 alias dev_control="tmuxp load src/g_stream/launch/dev_pc_264_control.yaml"
